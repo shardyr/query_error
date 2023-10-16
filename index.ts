e1e2e4b5d0af016e2974524e4473d2e9a3e4da1c
shardyr <shardyr85@gmail.com>
2023-10-16 21:32:29 +0200

    remove comments

--- index.ts
+++ index.ts
@@ -1,9 +1,3 @@
-// import TelegramBot from "node-telegram-bot-api";
-// import Web3 from "web3";
-// import * as tdl from "tdl";
-// import { getTdjson } from "prebuilt-tdlib";
-
-// upstash
 import { Redis } from "@upstash/redis";
 import { Query } from "@upstash/query";
 
